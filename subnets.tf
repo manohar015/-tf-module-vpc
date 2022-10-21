fde3f678b95eaef3d182f0ce765e08e3fe26ae3d
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 06:53:59 +0530

    My first push

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "main" {
-  count      = lenght(var.PUBLIC_SUBNET_CIDR)
+  count      = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
 
