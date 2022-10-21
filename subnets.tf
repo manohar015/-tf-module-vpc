98c9cb9b7f314fb1f1d7d40791a124bff64e3aa5
mvpc-11232 <mvp2612@hotmail.com>
2022-10-21 06:58:02 +0530

    Auto Push Commit

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,10 @@
 resource "aws_subnet" "main" {
-  count      = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  count               = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id              = aws_vpc.main.id
+  cidr_block          = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone   = element(var.AZ, count.index)
 
   tags = {
-    Name = "
+    Name = "${var.ENV}"
   }
 }
