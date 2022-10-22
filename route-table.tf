8c27cf7986e8f8e68289d59bc8db42dafe4401fd
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 06:28:54 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -33,11 +33,18 @@ resource "aws_route_table" "private-rt" {
 
 # accepts only the traffic from default VPC CIDR
   route {
-    cidr_block                = var.VPC_CIDR
+    cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
   tags = {
     Name = "${var.ENV}-pub-route-table"
   }
+}
+
+# Attaches private route-table to private subnets
+resource "aws_route_table_association" "private-rt-association" {
+  count          = length(aws_subnet.private.*.id)
+  subnet_id      = element(ws_subnet.private.*.id_subnet, count.index)
+  route_table_id = aws_route_table.private-rt.id
 }
