be3566315e22a551d2666c665abd401f80281c72
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 07:11:53 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -38,8 +38,8 @@ resource "aws_route_table" "private-rt" {
   }
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw.igw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   tags = {
