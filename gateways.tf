5a01aabdeb826bf8d90377dddcee202b28046e1c
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 07:09:27 +0530

    Auto Push Commit

--- gateways.tf
+++ gateways.tf
@@ -20,8 +20,7 @@ resource "aws_nat_gateway" "ngw" {
   tags = {
     Name = "${var.ENV}-ngw"
   }
-
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_internet_gateway.igw]
 }
