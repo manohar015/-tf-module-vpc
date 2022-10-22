4db517e298865e1e2a9f5cc5dc102f73b74c8371
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 06:34:19 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -9,7 +9,7 @@ resource "aws_route_table" "public-rt" {
   }
 
   route {
-    cidr_block                = var.VPC_CIDR
+    cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
