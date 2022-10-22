8f47c74ff814431e5baefcd22cbb4a345e750816
mvpc-11232 <mvp2612@hotmail.com>
2022-10-22 06:29:58 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -21,7 +21,7 @@ resource "aws_route_table" "public-rt" {
 # Attaches public route-table to public subnets
 resource "aws_route_table_association" "public-rt-association" {
   count          = length(aws_subnet.public.*.id)
-  subnet_id      = element(ws_subnet.public.*.id_subnet, count.index)
+  subnet_id      = element(aws_subnet.public.*.id_subnet, count.index)
   route_table_id = aws_route_table.public-rt.id
 }
 
