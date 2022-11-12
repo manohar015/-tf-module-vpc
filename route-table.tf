921e278474f28b1717003e43e6da4511f6e71ab9
Manohar <manohar015@gmail.com>
2022-11-12 21:45:31 +0530

    Added my Comments

--- route-table.tf
+++ route-table.tf
@@ -8,10 +8,7 @@ resource "aws_route_table" "public-rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
-  route {
-    cidr_block                = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  }
+
 
   tags = {
     Name = "${var.ENV}-pub-route-table"
