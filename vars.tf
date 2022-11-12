cb9b1cb6fc641371525c299c75e2c9e6c8e49256
Manohar <manohar015@gmail.com>
2022-11-12 22:20:30 +0530

    Added my Comments

--- vars.tf
+++ vars.tf
@@ -3,6 +3,6 @@ variable "ENV" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "AZ" {}
-variable "DEFAULT_VPC_ID" {
-  
-}
+variable "DEFAULT_VPC_ID" {}
+variable "DEFAULT_VPC_CIDR" {}
+
