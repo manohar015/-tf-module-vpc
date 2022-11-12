249c507a7603e4fe4d4da8bbda5532d3f61fdacb
Manohar <manohar015@gmail.com>
2022-11-12 22:10:50 +0530

    Added my Comments

--- route-table.tf
+++ route-table.tf
@@ -8,7 +8,10 @@ resource "aws_route_table" "public-rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
-
+ route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
 
   tags = {
     Name = "${var.ENV}-pub-route-table"
@@ -20,4 +23,24 @@ resource "aws_route_table_association" "public-rt-association" {
   count          = length(aws_subnet.public.*.id)
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public-rt.id
+}
+
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+# accepts only the traffic from default VPC CIDR
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+    tags = {
+        Name = "${var.ENV}-prv-route-table"
+    }
+}
+  
+resource "aws_route_table_association" "private-rt-association" {
+  count          = length(aws_subnet.private.*.id)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private-rt.id
 }
