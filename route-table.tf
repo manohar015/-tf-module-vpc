9e7740d8b9777aa59bab4dea8631cbdfaefc8846
Manohar <manohar015@gmail.com>
2022-11-12 21:57:16 +0530

    Added my Comments

--- route-table.tf
+++ route-table.tf
@@ -14,3 +14,10 @@ resource "aws_route_table" "public-rt" {
     Name = "${var.ENV}-pub-route-table"
   }
 }
+
+# Attaches public route-table to public subnets
+resource "aws_route_table_association" "public-rt-association" {
+  count          = length(aws_subnet.public.*.id)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public-rt.id
+}
