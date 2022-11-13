6594354ff36bdee290c6b1c1e353aecebc770f00
Manohar <manohar015@gmail.com>
2022-11-13 12:13:34 +0530

    Added my Comments

--- route-table.tf
+++ route-table.tf
@@ -46,4 +46,8 @@ resource "aws_route_table_association" "private-rt-association" {
 }
 
 # Adds route on the default vpc's public subnet route table.
-
+resource "aws_route" "r" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
