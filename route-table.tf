77f679c3c962543f20e13957086d78c95c8a1294
Manohar <manohar015@gmail.com>
2022-11-13 14:16:48 +0530

    Added my Comments

--- route-table.tf
+++ route-table.tf
@@ -34,6 +34,11 @@ resource "aws_route_table" "private-rt" {
     cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
+
+    route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
     tags = {
         Name = "${var.ENV}-prv-route-table"
     }
