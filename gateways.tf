3c53581a4356690b41302860a45e35124c88ed0d
Manohar <manohar015@gmail.com>
2022-12-15 19:26:48 +0530

    Added my Comments

--- gateways.tf
+++ gateways.tf
@@ -24,4 +24,4 @@ resource "aws_nat_gateway" "ngw" {
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
-}
+} 
