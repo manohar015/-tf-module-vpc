c93abbb963c85fb7d8e7f988fdec85197b6e9865
Manohar <manohar015@gmail.com>
2022-12-17 09:38:10 +0530

    Added my Comments

--- vars.tf
+++ vars.tf
@@ -6,3 +6,8 @@ variable "AZ" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_RT" {}
+variable "PRIVATE_HOSTEDZONE_ID" {}
+variable "PRIVATE_HOSTEDZONE_ID" {}
+variable "PRIVATE_HOSTEDZONE_NAME" {}  
+variable "PUBLIC_HOSTEDZONE_ID" {}
+variable "PUBLIC_HOSTEDZONE_NAME" {}  
